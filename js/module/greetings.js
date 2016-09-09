52fe0b95e1a074b5f06729f6755d0b96a688ea49
silence <silence@silence.local>
2016-09-09 14:00:41 +0800

    module.exports 与 exports理解

---
+++ js/module/greetings.js
@@ -0,0 +1,16 @@
+/**
+ * @author [https://github.com/silence717]
+ * @since  2016-09-08
+ */
+// greetings.js
+// var exports = module.exports = {};
+
+exports.sayHelloInEnglish = function() {
+    return "HELLO";
+};
+
+exports.sayHelloInSpanish = function() {
+    return "Hola";
+};
+
+module.exports = "Bonjour";
---
+++ js/module/main.js
@@ -0,0 +1,12 @@
+/**
+ * @author [https://github.com/silence717]
+ * @since  2016-09-08
+ */
+var greetings = require("./greetings.js");
+// "Hello"
+// console.log(greetings.sayHelloInEnglish());
+
+// "Hola"
+// console.log(greetings.sayHelloInSpanish());
+
+console.log(greetings);
